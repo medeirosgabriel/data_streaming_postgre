a81ab7163e31b28134c6939624687e70f3c373fe
davibss <davi.sousa@ccc.ufcg.edu.br>
2023-10-08 21:07:42 -0300

    feat: add timed metrics to notify and listen methods

--- orderservice/src/main/java/com/medeirosgabriel/orderservice/postgres_async/NotifierService.java
+++ orderservice/src/main/java/com/medeirosgabriel/orderservice/postgres_async/NotifierService.java
@@ -2,32 +2,46 @@ package com.medeirosgabriel.orderservice.postgres_async;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.medeirosgabriel.orderservice.micrometer.CustomMetricsService;
 import com.medeirosgabriel.orderservice.model.Order_;
+import io.micrometer.core.annotation.Timed;
 import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.postgresql.PGConnection;
 import org.postgresql.PGNotification;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.sql.Connection;
 import java.util.function.Consumer;
 
 @Slf4j
-
+@Component
 public class NotifierService {
     public static final String ORDERS_CHANNEL = "orders";
     private final JdbcTemplate tpl;
+
+    @Autowired
+    CustomMetricsService customMetricsService;
 
     public NotifierService(JdbcTemplate tpl) {
         this.tpl = tpl;
     }
 
     @Transactional
+    @Timed(
+            value = CustomMetricsService.NOTIFY_ORDER_UPDATE,
+            description = "Time taken to notify a change from an order update",
+            extraTags = {"broker","postgres"}
+    )
     public void notifyOrderCreated(Order_ order) {
         ObjectMapper objectMapper = new ObjectMapper();
         try {
             String orderString = objectMapper.writeValueAsString(order);
             tpl.execute("NOTIFY " + ORDERS_CHANNEL + ", '" + orderString + "'");
+//            customMetricsService.increaseNotifyCounter();
         } catch (JsonProcessingException e) {
             log.error("fail to write value to json");
         }
